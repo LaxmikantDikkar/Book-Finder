20bb54acf93c40fda5061eaa0ca294ad600179ef
LaxmikantDikkar <laxmikantdikkar3753@gmail.com>
2025-09-03 17:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bookRoutes from "./routes/books.js";
 import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
 
 dotenv.config();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
