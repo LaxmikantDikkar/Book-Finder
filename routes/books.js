e9f807f80b7d4ec3d543ae5389c0f576b694a48a
LaxmikantDikkar <laxmikantdikkar3753@gmail.com>
2025-09-03 16:14:33 +0530

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ import Favorite from "../models/Favorite.js";
 const router = express.Router();
 
 // 🔍 Search books
-router.get("/search", async (req, res) => {
+router.get("/", async (req, res) => {
 
   const { title, author, isbn, subject } = req.query;
   let query = "";
