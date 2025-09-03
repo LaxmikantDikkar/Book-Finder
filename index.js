aca3dfb8e9b791ae83d705b2f2bd09b39ac1a9d7
LaxmikantDikkar <laxmikantdikkar3753@gmail.com>
2025-09-03 16:53:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(()=> console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.use("/", bookRoutes);
+app.use("/api/books", bookRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
